ca3590b3e307a4b6610a0e013cc6b9b760dddfbb
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-07-02 20:53:37 +0545

    01 number of cpu used complete

--- _01_number_of_cpu_used.py
+++ _01_number_of_cpu_used.py
@@ -1,1 +1,12 @@
 # Write a Python program to find out the number of CPUs used.
+
+import multiprocessing
+import os
+
+# Use 'multiprocessing.cpu_count()' to determine the number of available CPU cores.
+cpu_count = multiprocessing.cpu_count()
+os_cpu_count = os.cpu_count()
+
+# Print the number of CPU cores available on the system.
+print("Multiprocessing ",cpu_count)
+print("OS ",os_cpu_count)
