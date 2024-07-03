756caa8f247178f594d4f7197fcbcfc4d4cf0551
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-07-03 21:09:57 +0545

    02 cube and square complete

--- _02_cube_and_square.py
+++ _02_cube_and_square.py
@@ -2,14 +2,27 @@
 
 import multiprocessing
 
-def square(n):
-    pass
+def square(n,result):
+    result.value = n*n
 
-def cube(n):
-    pass
+def cube(n,result):
+    result.value = n*n*n
 
 def main():
-    pass
+    square_result = multiprocessing.Value('i')
+    cube_result = multiprocessing.Value('i')
+    
+    p1 = multiprocessing.Process(target=square,args=(4,square_result))
+    p2 = multiprocessing.Process(target=cube,args=(2,cube_result))
+    
+    p1.start()
+    p2.start()
+    
+    p1.join()
+    p2.join()
+    
+    print(f"Square: {square_result.value}")
+    print(f"Cube: {cube_result.value}")
 
 if __name__ == "__main__":
     main()
