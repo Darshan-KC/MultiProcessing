488ad9b2c4a9796a13c0b71921c5babed7da8caa
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-07-03 20:32:23 +0545

    02 cube and square added

---
+++ _02_cube_and_square.py
@@ -0,0 +1,15 @@
+# Write a program to find cube and square using multiprocessing
+
+import multiprocessing
+
+def square(n):
+    pass
+
+def cube(n):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
