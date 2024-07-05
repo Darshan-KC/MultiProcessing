75a5a8488a1309333140aac9c6c95035b1b3c331
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-07-05 20:19:50 +0545

    04 communication between process 1 complete

--- _04_communication_between_process_1.py
+++ _04_communication_between_process_1.py
@@ -8,8 +8,24 @@ import multiprocessing
 
 result = []
 
-def square_list(mylist):
-    pass
+def square_list(mylist :list) ->None:
+    """ 
+    function to square a given list 
+    """
+    result = [x*x for x in mylist]
+    
+    print("Result (in process p1): {}".format(result))
 
 if __name__ == "__main__":
-    pass
+    # input list 
+    mylist = [1,2,3,4]
+    
+    # Creating new process
+    p1 = multiprocessing.Process(target=square_list,args=(mylist,))
+    
+    p1.start()
+    
+    p1.join()    
+
+    # print global result list 
+    print("Result(in main program): {}".format(result))
