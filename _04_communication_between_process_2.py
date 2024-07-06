923275fa12cd17d4d218a6b70a85abe8e2aa8db0
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-07-06 20:41:15 +0545

    04 communication between process 2 complete

--- _04_communication_between_process_2.py
+++ _04_communication_between_process_2.py
@@ -4,3 +4,46 @@
 # Given below is a simple example showing use of Array and Value for sharing data between processes.
 
 import multiprocessing
+
+def square_list(mylist, result, square_sum):
+    """ 
+    function to square a given list 
+    arg: mylist -> list, result -> square of list, square_sum -> sum of square of list
+    """
+    for idx,num in enumerate(mylist):
+        result[idx] = num*num
+        
+    square_sum.value = sum(result)
+    
+    # print result Array 
+    print("Result(in process p1): {}".format(result[:])) 
+  
+    # print square_sum Value 
+    print("Sum of squares(in process p1): {}".format(square_sum.value)) 
+
+def main() ->None:
+    """
+    This is main function
+    """
+    mylist = [1,2,3,4]
+    
+    # Creating array of int datatypes with space for 4 integers
+    result = multiprocessing.Array('i',4)
+    
+    # Creating Value of int data type
+    square_sum = multiprocessing.Value('i') # you can use 'd' for float (double)
+    
+    p1 = multiprocessing.Process(target=square_list,args=(mylist,result,square_sum))
+
+    p1.start()
+    
+    p1.join()
+
+    # print result array 
+    print("Result(in main program): {}".format(result[:])) 
+  
+    # print square_sum Value 
+    print("Sum of squares(in main program): {}".format(square_sum.value))
+
+if __name__ == "__main__":
+    main()
