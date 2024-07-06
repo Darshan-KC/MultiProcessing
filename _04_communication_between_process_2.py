27df21ba6c200d094f931a6b2b806502a6f8b52a
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-07-06 20:31:32 +0545

    04 communication between process 2 added

---
+++ _04_communication_between_process_2.py
@@ -0,0 +1,6 @@
+# multiprocessing module provides Array and Value objects to share data between processes.
+# 1. Array: a ctypes array allocated from shared memory.
+# 2. Value: a ctypes object allocated from shared memory.
+# Given below is a simple example showing use of Array and Value for sharing data between processes.
+
+import multiprocessing
