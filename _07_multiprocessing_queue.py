6e1f05b0f14d308cf4ec91f49bba8e3b40859d3a
Darshan-KC <darshanbahadurchhetri@gmail.com>
2024-07-08 20:44:15 +0545

    07 multiprocessing queue

---
+++ _07_multiprocessing_queue.py
@@ -0,0 +1,4 @@
+# Queue : A simple way to communicate between process with multiprocessing is to use a Queue to pass messages back and forth. Any Python object can pass through a Queue.
+# Note: The multiprocessing.Queue class is a near clone of queue.Queue.
+
+import multiprocessing
